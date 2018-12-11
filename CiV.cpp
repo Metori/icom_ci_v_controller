ccdcc808a569eb5176ff24e22838e114bca8d863
metori <artem092@mail.ru>
2018-12-11 22:21:21 +1000

    CiV protocol implementation development

--- CiV.cpp
+++ CiV.cpp
@@ -1,8 +1,36 @@
 #include "CiV.h"
 
-#define MSG_PREAMBLE1 0xFF
-#define MSG_PREAMBLE2 0xFF
+#define MSG_PREAMBLE 0xFF //two preambles start a CI-V message
 #define MSG_END_CODE 0xFD
 
 #define BROADCAST_ADDR 0x00
+
+void CCiV::send(uint8_t* payload, uint8_t size) {
+  uint8_t dataSize = size + 5;
+  uint8_t data[dataSize];
+  data[0] = MSG_PREAMBLE;
+  data[1] = MSG_PREAMBLE;
+  data[2] = mRadioAddr;
+  data[3] = mControllerAddr;
+
+  for (int i = 0; i < size; i++) {
+    data[i + size] = payload[i];
+  }
+
+  data[dataSize - 1] = MSG_END_CODE;
+
+  Serial.write(data, dataSize);
+}
+
+size_t CCiV::recv(uint8_t* buffer, uint8_t size) {
+  size_t ret = 0;
+
+  if (Serial.available()) {
+    while (Serial.available()) {
+
+    }
+  }
+
+  return ret;
+}
 
--- CiV.h
+++ CiV.h
@@ -3,6 +3,8 @@
 
 #include "Arduino.h"
 
+#define MSG_MAX_SIZE 12
+
 class CCiV {
 public:
   CCiV(uint8_t radioAddr, uint8_t controllerAddr, uint16_t baudRate)
@@ -17,10 +19,14 @@ public:
 
 private:
   void send(uint8_t* payload, uint8_t size);
+  size_t recv(uint8_t* buffer, uint8_t size);
 
   uint8_t mRadioAddr;
   uint8_t mControllerAddr;
   uint16_t mBaudRate;
+
+  uint8_t mInMsg[MSG_MAX_SIZE];
+  uint8_t mInOffset = 0;
 
 };
 
