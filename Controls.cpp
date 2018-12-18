1e984a0ca11ad3bc39350ecb9fd0fe1947ae25b1
metori <artem092@mail.ru>
2018-12-18 12:17:01 +1000

    Controls small rework. get rid of init()

--- Controls.cpp
+++ Controls.cpp
@@ -1,9 +1,21 @@
 #include "Controls.h"
 
-void CControls::init() {
-  for(int i = 0; i < mCount; i++) {
-    pinMode(mPins[i], INPUT_PULLUP);
+CControls::CControls(SButton* buttons, uint8_t count) {
+  mPins = new int[count];
+  mDebouncers = new Bounce[count];
+  mCount = count;
+  
+  for(int i=0; i<count; i++) {
+    SButton btn = buttons[i];
+    
+    pinMode(btn.pin, INPUT_PULLUP);
+    mDebouncers[i].attach(btn.pin);
+    mDebouncers[i].interval(btn.debounceMs);
   }
+}
+CControls::~CControls() {
+  delete[] mPins;
+  delete[] mDebouncers;
 }
 
 int CControls::update() {
--- Controls.h
+++ Controls.h
@@ -11,25 +11,9 @@ typedef struct {
 
 class CControls {
 public:
-  CControls(SButton* buttons, uint8_t count) {
-    mPins = new int[count];
-    mDebouncers = new Bounce[count];
-    mCount = count;
-    
-    for(int i=0; i<count; i++) {
-      SButton btn = buttons[i];
-      
-      pinMode(btn.pin, INPUT_PULLUP);
-      mDebouncers[i].attach(btn.pin);
-      mDebouncers[i].interval(btn.debounceMs);
-    }
-  }
-  ~CControls() {
-    delete[] mPins;
-    delete[] mDebouncers;
-  }
+  CControls(SButton* buttons, uint8_t count);
+  ~CControls();
 
-  void init();
   int update();
 
 private:
